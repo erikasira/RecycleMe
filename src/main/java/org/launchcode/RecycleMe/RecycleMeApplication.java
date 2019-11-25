d3e479a00068e8e32b9ba9414b8b4081193a9892
Erika <erika.sira@gmail.com>
2019-11-25 11:34:05 -0500

    initial commit

---
+++ src/main/java/org/launchcode/RecycleMe/RecycleMeApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.RecycleMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecycleMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/RecycleMe/RecycleMeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.RecycleMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
